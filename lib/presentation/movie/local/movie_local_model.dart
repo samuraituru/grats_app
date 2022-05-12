2955af8796a88fc6065f6e63ed34b09c8d089c80
samuraituru <samuraituru.dev@gmail.com>
2022-05-12 23:56:59 +0900

    third commit

--- lib/presentation/movie/local/movie_local_model.dart
+++ lib/presentation/movie/local/movie_local_model.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+class Task {
+  Task(this.tasks);
+  String tasks;
+}
 class MovieLocalModel extends ChangeNotifier {
+  List<Task>? tasks;
+  String? task;
 
+  Future addbook() async {
+    if (task == null || task == "") {
+      throw 'タイトルが入力されていません';
+    }
+    notifyListeners();
+  }
+  void fetchBookList() async {
+    this.tasks = tasks;
+    notifyListeners();
+  }
 }
